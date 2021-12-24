5367c145f0528c018ad4bd48e60d0f9308876faf
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-12-24 01:19:18 -0300

    Validations.

--- src/UserCreate.php
+++ src/UserCreate.php
@@ -17,24 +17,60 @@ class UserCreate extends Command
     protected function configure()
     {
         $this->setName('user:create')
-        ->addArgument('name', InputArgument::REQUIRED, "User's first name")
-        ->addArgument('lastName', InputArgument::REQUIRED, "User's last name")
-        ->addArgument('email', InputArgument::REQUIRED, "User's email")
-        ->addArgument('age', InputArgument::OPTIONAL, "User's age")
+            ->addArgument('name', InputArgument::REQUIRED, "User's first name")
+            ->addArgument('lastName', InputArgument::REQUIRED, "User's last name")
+            ->addArgument('email', InputArgument::REQUIRED, "User's email")
+            ->addArgument('age', InputArgument::OPTIONAL, "User's age")
             ->setDescription("Create a new user")
             ->setHelp("Create a new user");
     }
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $user = new User();
-        $user->setName($input->getArgument('name'));
-        $user->setLastName($input->getArgument('lastName'));
-        $user->setEmail($input->getArgument('email'));
-        $user->setAge($input->getArgument('age'));
+        $name = $input->getArgument('name');
+        $lastName = $input->getArgument('lastName');
+        $email = $input->getArgument('email');
+        $age = intval($input->getArgument('age'));
+
+        if (strlen($name) < 2) {
+            $output->writeln("Type a name longer than 1");
+            return;
+        }
+        if (strlen($name) > 35) {
+            $output->writeln("Type a name shorter than 35");
+            return;
+        }
+
+        if (strlen($lastName) < 2) {
+            $output->writeln("Type a last name longer than 1");
+            return;
+        }
+        if (strlen($lastName) > 35) {
+            $output->writeln("Type a last name shorter than 35");
+            return;
+        }
+        if ($age < 2) {
+            $output->writeln("Age must be between 2 and 35");
+            return;
+        }
+        if ($age > 150) {
+            $output->writeln("Age must be between 2 and 35");
+            return;
+        }
+        
+        if (!$this->validaEmail($email)) {
+            $output->writeln("Type a valid mail");
+            return;
+        }
+
+        $user->setName($name);
+        $user->setLastName($lastName);
+        $user->setEmail($email);
+        $user->setAge($age);
 
         $userDao = new UserDAO();
         if ($userDao->insert($user)) {
-            
+
             $user->setId($userDao->getConnection()->lastInsertId());
             $user = json_encode(array(
                 'id' => $user->getId(),
@@ -46,6 +82,19 @@ class UserCreate extends Command
             $output->writeln(json_encode($user));
         } else {
             $output->writeln("Database Error");
+        }
+    }
+
+    function validaEmail($email)
+    {
+        $conta = "^[a-zA-Z0-9\._-]+@";
+        $domino = "[a-zA-Z0-9\._-]+.";
+        $extensao = "([a-zA-Z]{2,4})$";
+
+        if(preg_match("/^([[:alnum:]_.-]){3,}@([[:lower:][:digit:]_.-]{3,})(.[[:lower:]]{2,3})(.[[:lower:]]{2})?$/", $email)) {
+            return true;
+        }else{
+            return false;
         }
     }
 }
